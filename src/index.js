ba79b65ffb6e07599ec4694e2c51f447b5ca3709
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-03-17 22:50:08 +0200

    task10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,81 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './js/fetchCountries';
+
+var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  countryListField: document.querySelector('.country-list'),
+  countryInfoField: document.querySelector('.country-info'),
+};
+
+refs.input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(event) {
+  event.preventDefault();
+  const inputValue = event.target.value.trim();
+  if (inputValue === '') {
+    refs.countryListField.innerHTML = '';
+    refs.countryInfoField.innerHTML = '';
+    return;  
+  }
+
+  fetchCountries(inputValue).then(data => {
+    if (data.length >= 10 && inputValue !== '') {
+      refs.countryListField.innerHTML = '';
+      Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+      return;}
+    
+    else if (data.length >= 2 && data.length < 10) {
+      refs.countryInfoField.innerHTML = '';
+      const manyCountryMarkup = onManyCountryMarkup(data);
+      refs.countryListField.innerHTML = manyCountryMarkup;
+      return;
+    }
+      
+    else if (data.length < 2) {
+    refs.countryListField.innerHTML = '';
+    onOneCoutryMarkup(data);
+    const oneCountryMarkup = onOneCoutryMarkup(data);
+    refs.countryInfoField.innerHTML = oneCountryMarkup;
+    return;
+    }   
+  });  
+
+  
+
+function onManyCountryMarkup(data) {
+    return data.map(({ name, flags }) => {
+    return `
+      <li class='country-item'>
+      <img src="${flags.svg}" alt="${flags.alt}" width="100" height="60">
+      <h1> ${name.official} </h1>
+      </li>    
+    `;
+  }).join('');
+  
+};
+
+
+function onOneCoutryMarkup(data) {
+  return data.map(({ name, capital, population, flags, languages }) => {
+    return `
+    <div>
+      <img src="${flags.svg}" alt="${flags.alt}" width="100" height="60">
+      <h1>${name.official}</h1>
+    </div>
+      <h3>Capital: ${capital} </h3>
+      <h3>Population: ${population} </h3>
+      <h3>Languages: ${Object.values(languages)} </h3>
+  `;
+  }).join('');  
+};
+
+
+
+
+
+
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,13 @@
+export const fetchCountries = function fetchCountries(name) {
+
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => response.json())
+    .then(data => {
+      return data
+    });
+};
+
+
+
